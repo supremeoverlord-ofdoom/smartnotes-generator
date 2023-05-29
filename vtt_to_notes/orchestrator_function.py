f06b65f06b96cbb05b01c964953c0449e193dedf
sierra.mcdowell <sierra.mcdowell@cuusoo.com.au>
2023-05-29 14:22:57 +0800

    auto archive input files

--- vtt_to_notes/orchestrator_function.py
+++ vtt_to_notes/orchestrator_function.py
@@ -4,6 +4,7 @@ from sentence_transformers import SentenceTransformer
 import glob #for file name reading
 import os
 import datetime
+import shutil #for auto moving files
 
 #create an instance of the class
 processor = SubtitleProcessor()
@@ -24,15 +25,20 @@ else:
 #get filenames of all vtt files in input folder
 filenames = glob.glob('input/*.vtt')
 # print(filenames)
+#store location of files to go once processed
+archive_destination = 'input/processed_files'
 
 # looping SubtitleProcessor through all vtt files in input folder
 for input_file_path in filenames: 
     base_name = os.path.splitext(os.path.basename(input_file_path))[0]  # Extract base file name of input file
     current_date = datetime.datetime.now().strftime("%Y%m%d") # Get current date
-    # output_file_path = 'output/notes7.txt'
     output_file_path = f'output/notes_{base_name}_{current_date}.txt' #concat unqique file name for output_file
     processor.process_subtitles(input_file_path, output_file_path) #apply function process_subtitles 
     print(f"subtitle file: {input_file_path} successfully processed")
+    shutil.move(input_file_path, archive_destination) #move input files into an archive folder
 print(f"all .vtt files in input folder processed, bye bye")
 
 #python3 orchestrator_function.py
+
+
+
