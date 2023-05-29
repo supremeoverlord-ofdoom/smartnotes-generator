ea210ba2455071b9f66de650495d3dd002044777
sierra.mcdowell <sierra.mcdowell@cuusoo.com.au>
2023-05-29 16:15:35 +0800

    auto batch output into timestamped folder
    bundle notes into folder with date and time stamp in output folder to keep things tidy when running script multiple times

--- vtt_to_notes/orchestrator_function.py
+++ vtt_to_notes/orchestrator_function.py
@@ -3,7 +3,7 @@ from subtitle_processor import SubtitleProcessor
 from sentence_transformers import SentenceTransformer
 import glob #for file name reading
 import os
-import datetime
+from datetime import datetime
 import shutil #for auto moving files
 
 #create an instance of the class
@@ -28,13 +28,16 @@ filenames = glob.glob('input/*.vtt')
 #store location of files to go once processed
 archive_destination = 'input/processed_files'
 
+#make unique folder to bundle output notes as a processed batch
+date_time = datetime.now().strftime("%m%d%Y_%H-%M-%S") #get date and time for output folder for batch
+output_batch_folder = f"output/batch_processed_{date_time}" 
+os.makedirs(output_batch_folder) 
+    
 # looping SubtitleProcessor through all vtt files in input folder
 for input_file_path in filenames: 
     base_name = os.path.splitext(os.path.basename(input_file_path))[0]  # Extract base file name of input file
-    current_date = datetime.datetime.now().strftime("%Y%m%d") # Get current date
-    date_time = datetime.datetime.now.strftime("%m/%d/%Y_%H:%M:%S") #get date and time for output folder for batch
-    output_file = 
-    output_file_path = f'output/notes_{base_name}_{current_date}.txt' #concat unqique file name for output_file
+    current_date = datetime.now().strftime("%Y%m%d") # Get current date
+    output_file_path = f'{output_batch_folder}/notes_{base_name}_{current_date}.txt' #concat unqique file name for output_file
     processor.process_subtitles(input_file_path, output_file_path) #apply function process_subtitles 
     print(f"subtitle file: {input_file_path} successfully processed")
     shutil.move(input_file_path, archive_destination) #move input files into an archive folder
